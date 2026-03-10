0b19ccc8dd8f45d4a7dbb33fb10ed8a45934ef2b
Igor Laur <igor.laur2@gmail.com>
2026-03-09 21:17:30 -0300

    Add assetPrefix to fix styles when proxied via escalaecommerce.com.br/blog

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: 'https://blog-ecommerce-one.vercel.app',
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
